44085b930e85b4fc541e889d1a2b9496b0f22d81
Josh Mahony <jalmahony@gmail.com>
2014-02-24 22:28:06 +0000

    Create HTMLLine class and its JUnit tests

---
+++ src/main/java/com/joshmahony/HTMLLine.java
@@ -0,0 +1,7 @@
+package com.joshmahony;
+
+/**
+ * Created by joshmahony on 24/02/2014.
+ */
+public class HTMLLine {
+}
---
+++ src/test/java/com/joshmahony/HTMLLineTest.java
@@ -0,0 +1,32 @@
+package com.joshmahony;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Created by joshmahony on 24/02/2014.
+ */
+public class HTMLLineTest extends TestCase {
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public HTMLLineTest( String testName ) {
+
+        super( testName );
+
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+
+        return new TestSuite( HTMLLineTest.class );
+
+    }
+
+}
