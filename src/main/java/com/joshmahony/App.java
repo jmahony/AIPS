abc0a62b0ab5f46c09c39d34c07653633a54f1a6
Josh Mahony <jalmahony@gmail.com>
2014-02-24 22:46:36 +0000

    Add in tokenise method, this simply uses Open NLP and the en token training file to tokenise a given string

--- src/main/java/com/joshmahony/App.java
+++ src/main/java/com/joshmahony/App.java
@@ -1,5 +1,12 @@
 package com.joshmahony;
 
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Hello world!
  *
@@ -9,5 +16,37 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    private String[] tokenize(String sentence) {
+
+        InputStream modelIn = null;
+
+        String[] tokens = null;
+        try {
+
+            modelIn = getClass().getResourceAsStream("/en-token.bin");
+
+            TokenizerModel model = new TokenizerModel(modelIn);
+
+            Tokenizer tokenizer = new TokenizerME(model);
+
+            tokens = tokenizer.tokenize(sentence);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (modelIn != null) {
+                try {
+                    modelIn.close();
+                }
+                catch (IOException e) {
+                }
+            }
+        }
+
+        return tokens;
+
     }
 }
