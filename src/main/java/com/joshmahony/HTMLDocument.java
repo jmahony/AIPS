3c8c812b3e00f5cbd9739d5fc4e94d3d2498897c
Josh Mahony <jalmahony@gmail.com>
2014-02-25 10:50:47 +0000

    Add in HTMLDocument class, this will store the original HTML and carry out processing on the HTML.

---
+++ src/main/java/com/joshmahony/HTMLDocument.java
@@ -0,0 +1,24 @@
+package com.joshmahony;
+
+/**
+ * Created by josh on 25/02/14.
+ */
+public class HTMLDocument {
+
+    /**
+     * Original HTML line
+     */
+    public final String html;
+
+    /**
+     * Takes the HTML line
+     *
+     * @param _html
+     */
+    public HTMLDocument(String _html) {
+
+        html = _html;
+
+    }
+
+}
