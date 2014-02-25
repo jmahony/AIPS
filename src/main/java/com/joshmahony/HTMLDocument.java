730bd84c113a3cd059b8bd6da74f2f4150cf105b
Josh Mahony <jalmahony@gmail.com>
2014-02-25 13:17:34 +0000

    Implement getBody, add some unit testd and refactor unit tests

--- src/main/java/com/joshmahony/HTMLDocument.java
+++ src/main/java/com/joshmahony/HTMLDocument.java
@@ -1,5 +1,8 @@
 package com.joshmahony;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by josh on 25/02/14.
  */
@@ -22,6 +25,61 @@ public class HTMLDocument {
         html = _html.trim();
 
         htmlLines = getLines();
+
+    }
+
+    private String getBody() {
+
+        return HTMLDocument.getBody(html);
+
+    }
+
+    /**
+     *
+     * Returns the HTML between <body> & </body>
+     *
+     * @param _html
+     * @return
+     */
+    public static String getBody(String _html) {
+
+        // Get the index position of the last character of the <body> tag,
+        // There could be multiple opening body tags, we are looking for the first
+        // character of the first occurrence.
+        Pattern openingBody = Pattern.compile("<body.*>");
+
+        Matcher openingBodyMatcher = openingBody.matcher(_html);
+
+        int firstPos = 0,
+            lastPos  = _html.length();
+
+        while(openingBodyMatcher.find()) {
+
+            if (openingBodyMatcher.end() > firstPos) {
+
+                firstPos = openingBodyMatcher.end();
+
+            }
+
+        }
+
+        // Get the index position of the first character of the </body> tag
+        Pattern closingBody = Pattern.compile("</body>");
+
+        Matcher closingBodyMatcher = closingBody.matcher(_html);
+
+        while(closingBodyMatcher.find()) {
+
+            if (closingBodyMatcher.start() < lastPos) {
+
+                lastPos = closingBodyMatcher.start();
+
+            }
+
+        }
+
+        // Return the string between these two indices, and trim leading and trailing whitespace
+        return _html.substring(firstPos, lastPos).trim();
 
     }
 
--- src/test/java/com/joshmahony/HTMLDocumentTest.java
+++ src/test/java/com/joshmahony/HTMLDocumentTest.java
@@ -33,23 +33,20 @@ public class HTMLDocumentTest extends TestCase {
     }
 
     /**
-     * Test line count
+     *
+     * Make the right amount of lines are taken from the document, the test numbers are counted manually.
+     *
+     * @throws IOException
      */
     public void testGetLines() throws IOException {
 
-        String simpleHTML = IOUtils.toString(
-            this.getClass().getResourceAsStream("/simple.html"),
-            "UTF-8"
-        );
+        String simpleHTML = getResource("/simple.html");
 
         int lines1 = 64;
 
         assertEquals(lines1, HTMLDocument.getLines(simpleHTML).length);
 
-        String complexHTML = IOUtils.toString(
-            this.getClass().getResourceAsStream("/world-europe-26333587.html"),
-            "UTF-8"
-        );
+        String complexHTML = getResource("/world-europe-26333587.html");
 
         int lines2 = 2968;
 
@@ -57,6 +54,51 @@ public class HTMLDocumentTest extends TestCase {
 
     }
 
+    /**
+     *
+     * Test the simple HTML document to see if the body is correctly extracted
+     *
+     * @throws IOException
+     */
+    public void testSimpleGetBody() throws IOException {
 
+        String simpleHTML = getResource("/simple.html");
+
+        String simpleHTMLBody = getResource("/simpleBody.html");
+
+        assertEquals(simpleHTMLBody.trim(), HTMLDocument.getBody(simpleHTML));
+
+    }
+
+    /**
+     *
+     * Test the complex HTML document to see if the body is correctly extracted
+     *
+     * @throws IOException
+     */
+    public void testComplexGetBody() throws IOException {
+
+        String complexHTML = getResource("/world-europe-26333587.html");
+
+        String complexHTMLBody = getResource("/world-europe-26333587Body.html");
+
+        assertEquals(complexHTMLBody.trim(), HTMLDocument.getBody(complexHTML));
+
+    }
+
+    /**
+     * Returns a resource as a string
+     * @param resource
+     * @return
+     * @throws IOException
+     */
+    private String getResource(String resource) throws IOException {
+
+        return IOUtils.toString(
+            this.getClass().getResourceAsStream(resource),
+            "UTF-8"
+        );
+
+    }
 
 }
