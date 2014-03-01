3dd32a0ad340b66e9f5a87fc47c874c4f492dee4
Josh Mahony <jalmahony@gmail.com>
2014-03-01 22:48:30 +0000

    Change this to a unchecked exception

--- src/main/java/com/joshmahony/exceptions/InvalidKernelException.java
+++ src/main/java/com/joshmahony/exceptions/InvalidKernelException.java
@@ -3,8 +3,8 @@ package com.joshmahony.exceptions;
 /**
  * Created by josh on 28/02/14.
  */
-public class InvalidKernelException extends Exception{
-    
+public class InvalidKernelException extends IllegalArgumentException {
+
     public InvalidKernelException(String message) {
         
         super(message);
