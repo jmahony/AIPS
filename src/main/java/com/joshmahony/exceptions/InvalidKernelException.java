5e632e1eeee452e2429e82d19086e7de8616cdb5
Josh Mahony <jalmahony@gmail.com>
2014-03-01 19:20:36 +0000

    Add in smoothing method.

---
+++ src/main/java/com/joshmahony/exceptions/InvalidKernelException.java
@@ -0,0 +1,14 @@
+package com.joshmahony.exceptions;
+
+/**
+ * Created by josh on 28/02/14.
+ */
+public class InvalidKernelException extends Exception{
+    
+    public InvalidKernelException(String message) {
+        
+        super(message);
+        
+    }
+    
+}
