8a1364b3b1122992357363f8142e353d7510df69
Josh Mahony <jalmahony@gmail.com>
2014-03-28 09:15:59 +0000

    Increase test coverage. Code branches on recall and precision only tested both empty or neither empty. New tests added to cover this.

--- src/test/java/com/joshmahony/RelevancyTest.java
+++ src/test/java/com/joshmahony/RelevancyTest.java
@@ -20,13 +20,13 @@ public class RelevancyTest {
     @Test public void testFMeasureBasic() {
 
         double precision = 100.00d;
-        
+
         double recall = 63.15d;
-        
+
         double expected = 77.41342323;
-       
+
         assertEquals(expected, Relevancy.fMeasure(precision, recall), C.DOUBLE_TEST_DELTA);
-        
+
     }
 
     /**
@@ -115,13 +115,13 @@ public class RelevancyTest {
         double precision = 0.00d;
 
         double recall = 50.00d;
-        
+
         double expected = 0.00d;
 
         assertEquals(expected, Relevancy.fMeasure(precision, recall), C.DOUBLE_TEST_DELTA);
 
     }
-    
+
     /**
      * Test the f measure method with recall 0
      */
@@ -141,11 +141,11 @@ public class RelevancyTest {
      * Test precision and recall basic
      */
     @Test public void testPrecisionRecallBasic() {
-        
+
         double expectedPrecision, expectedRecall;
 
         Set<Integer> returned, relevant;
-        
+
         relevant = new HashSet<Integer>(){{
 
             add(3);
@@ -159,21 +159,21 @@ public class RelevancyTest {
             add(23);
             add(24);
             add(29);
-            
+
         }};
-        
+
         // ------- Test One -------
         returned = new HashSet<Integer>(){{
-            
+
             add(0); // wrong
             add(3); // right
             add(4); // right
             add(6); // wrong
             add(7); // wrong
-            
+
         }};
 
-        expectedPrecision = 40.00d;        
+        expectedPrecision = 40.00d;
         assertEquals(expectedPrecision, Relevancy.precision(relevant, returned), C.DOUBLE_TEST_DELTA);
 
         expectedRecall = 18.1818d;
@@ -218,30 +218,70 @@ public class RelevancyTest {
 
         expectedRecall = 100.00d;
         assertEquals(expectedRecall, Relevancy.recall(relevant, returned), C.DOUBLE_TEST_DELTA);
-        
+
     }
 
     /**
      * Test precision empty set
      */
-    @Test public void testPrecisionEmpty() {
-        
+    @Test public void testPrecisionBothEmpty() {
+
         Set<Integer> returned, relevant;
-        
+
         returned = new HashSet<Integer>();
 
         relevant = new HashSet<Integer>();
-        
+
         double precision = Relevancy.precision(relevant, returned);
-        
+
         assertEquals(0.00d, precision, C.DOUBLE_TEST_DELTA);
-        
+
     }
-    
+
+    /**
+     * Test precision empty set
+     */
+    @Test public void testPrecisionReturnedEmpty() {
+
+        Set<Integer> returned, relevant;
+
+        returned = new HashSet<Integer>();
+
+        relevant = new HashSet<Integer>(){{
+               add(1);
+        }};
+
+        double precision = Relevancy.precision(relevant, returned);
+
+        assertEquals(0.00d, precision, C.DOUBLE_TEST_DELTA);
+
+    }
+
+
+    /**
+     * Test precision empty set
+     */
+    @Test public void testPrecisionRelevantEmpty() {
+
+        Set<Integer> returned, relevant;
+
+        returned = new HashSet<Integer>(){{
+            add(1);
+        }};
+
+        relevant = new HashSet<Integer>();
+
+        double precision = Relevancy.precision(relevant, returned);
+
+        assertEquals(0.00d, precision, C.DOUBLE_TEST_DELTA);
+
+    }
+
+
     /**
      * Test recall empty set
      */
-    @Test public void testRecallEmpty() {
+    @Test public void testRecallBothEmpty() {
 
         Set<Integer> returned, relevant;
 
@@ -255,13 +295,54 @@ public class RelevancyTest {
 
     }
 
+
+    /**
+     * Test recall empty set
+     */
+    @Test public void testRecallReturnedEmpty() {
+
+        Set<Integer> returned, relevant;
+
+        returned = new HashSet<>();
+
+        relevant = new HashSet<Integer>(){{
+            add(1);
+        }};
+
+        double recall = Relevancy.recall(relevant, returned);
+
+        assertEquals(0.00d, recall, C.DOUBLE_TEST_DELTA);
+
+    }
+
+
+    /**
+     * Test recall empty set
+     */
+    @Test public void testRecallRelevantEmpty() {
+
+        Set<Integer> returned, relevant;
+
+        returned = new HashSet<Integer>(){{
+            add(1);
+        }};
+
+        relevant = new HashSet<Integer>();
+
+        double recall = Relevancy.recall(relevant, returned);
+
+        assertEquals(0.00d, recall, C.DOUBLE_TEST_DELTA);
+
+    }
+
+
     /**
      * Test all together
      */
     @Test public void testRecallPrecisionFMeasure() {
 
-        double expectedPrecision, 
-               expectedRecall, 
+        double expectedPrecision,
+               expectedRecall,
                expectedFMeasure,
                actualPrecision,
                actualRecall,
@@ -297,17 +378,17 @@ public class RelevancyTest {
             add(63); // wrong
             add(21); // wrong
             add(61); // wrong
-            
+
         }};
-        
+
         expectedPrecision = 40.00d;
-        actualPrecision = Relevancy.precision(relevant, returned);
+        actualPrecision   = Relevancy.precision(relevant, returned);
         assertEquals(expectedPrecision, actualPrecision, C.DOUBLE_TEST_DELTA);
-        
+
         expectedRecall = 36.36363636d;
-        actualRecall = Relevancy.recall(relevant, returned);
+        actualRecall   = Relevancy.recall(relevant, returned);
         assertEquals(expectedRecall, actualRecall, C.DOUBLE_TEST_DELTA);
-        
+
         expectedFMeasure = 38.09523809d;
         actualFMeasure   = Relevancy.fMeasure(actualPrecision, actualRecall);
         assertEquals(expectedFMeasure, actualFMeasure, C.DOUBLE_TEST_DELTA);
