8d1009d7f8146f66259610c711c0442087581cfa
Josh Mahony <jalmahony@gmail.com>
2014-04-15 16:44:18 +0100

    Add some comments and do some clean up.

--- src/main/java/es/cnewsbit/App.java
+++ src/main/java/es/cnewsbit/App.java
@@ -1,106 +1,48 @@
 package es.cnewsbit;
 
 import com.googlecode.flyway.core.Flyway;
-import es.cnewsbit.queriers.LuceneQuerier;
 import lombok.extern.log4j.Log4j2;
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.queryparser.classic.ParseException;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * Hello world!
- *
+ * Application entry point, simply migrates the database and starts the document
+ * processor
  */
 @Log4j2
 public class App {
 
-    private static Analyzer ANALYSER;
-
-    private static AtomicInteger count = new AtomicInteger(0);
-
-    private static long startTime = System.currentTimeMillis();
-
     public static void main(String[] args) {
 
-        // Perform database migrations
+        // Keep the MySQL database up to date and migrate the schema
         Flyway flyway = new Flyway();
         flyway.setDataSource(C.DB_NAME, C.DB_USER, C.DB_PASSWORD);
         flyway.migrate();
 
-
         try {
 
+            // Start the document processor
             DocumentProcessor dp = new DocumentProcessor();
 
+            // Rebuild truncate the current articles table and start from
+            // scratch
             dp.rebuild();
 
         } catch (IOException e) {
 
-            e.printStackTrace();
+            log.fatal(e.getMessage());
+
+            System.exit(-1);
 
         } catch (SQLException e) {
 
-            e.printStackTrace();
+            log.fatal(e.getMessage());
+
+            System.exit(-1);
 
         }
 
     }
-
-    public static void queryIndex() {
-
-        try {
-
-            LuceneQuerier lq = new LuceneQuerier(C.PATH_TO_INDEX, ANALYSER);
-
-            lq.query("rugby", 100);
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
-
-        } catch (ParseException e) {
-
-            e.printStackTrace();
-
-        }
-
-
-    }
-
-    public App() {
-
-
-
-
-
-        /**
-        try {
-
-            String html = ResourceLoader.asString(this, "/external/documents/uk-scotland-independence-devolution-idUKBREA1H1JM20140218");
-            String relevanceCSV = ResourceLoader.asString(this, "/local/relevance/uk-scotland-independence-devolution-idUKBREA1H1JM20140218.csv");
-
-            HTMLDocument doc = new HTMLDocument(html, new double[] {0.25, 0.5, 0.25});
-
-            Set<Integer> relevancySet = CSV.toRelevancySet(relevanceCSV, 0, 1);
-
-            RelevancyGenerator rg = new RelevancyGenerator(relevancySet, doc);
-
-            rg.generate(1.00d, 50.00d);
-
-            rg.printResults();
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-
-        }
-*/
-
-
-    }
-
 
 }
