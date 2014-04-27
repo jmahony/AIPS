ff5261a16e2f529fdc30d1e2353e34ba63cff460
Josh Mahony <jalmahony@gmail.com>
2014-04-27 14:39:27 +0100

    Create interfaces for content extractors, boiler pipe and TTR implementation.

---
+++ src/main/java/es/cnewsbit/extractors/TTRContentExtractor.java
@@ -0,0 +1,13 @@
+package es.cnewsbit.extractors;
+
+/**
+ * Created by joshmahony on 27/04/2014.
+ */
+public class TTRContentExtractor implements ContentExtractor {
+
+    @Override
+    public String extract(String s) {
+        return null;
+    }
+
+}
